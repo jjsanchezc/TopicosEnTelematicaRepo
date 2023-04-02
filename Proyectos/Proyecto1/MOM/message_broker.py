319a69a37c179c28bf5d7cf5fee135044696c108
jjsanchezc <jjsanchezc@eafit.edu.co>
2023-04-02 14:54:25 -0500

    elimine codigo que no funcionaba

--- Proyectos/Proyecto1/MOM/message_broker.py
+++ Proyectos/Proyecto1/MOM/message_broker.py
@@ -1,39 +1,13 @@
 from user import User
+from message_queue import MessageQueue
+from exchange import Exchange
 
 messages_queue = []
 username='JJ'   
 
 class MessageBroker:
     def __init__(self,topic_name,message):
-        self.exchange=self.Exchange()
-        self.message_queue=self.MessageQueue()
+        #self.exchange=Exchange()
+        self.message_queue=MessageQueue()
         self.topic_name=topic_name
         self.message=message
-    
-    class Exchange:
-        
-        #Methods for Topics
-        def create_topic(self,name):
-            pass
-        
-        def delete_topic(self,name):
-            pass
-        
-        
-        def publish_message(self,message,topic_name):
-            '''send message to queue
-
-            Args:
-                message (str): message to send
-                topic_name (str): topic name
-            '''
-        
-        
-    
-    
-    class Queue:
-        '''la cola almacena y luego entrega los mensajes a cada consumir
-        '''        
-        def __init__(self) -> None:
-            pass
-        
