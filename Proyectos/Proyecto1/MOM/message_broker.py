581828c01ae11f7eb912ad211cacf3beb4f3f361
portatil jjsanchezc <jjsanchezc@eafit.edu.co>
2023-04-05 17:24:17 -0500

    cambios

--- Proyectos/Proyecto1/MOM/message_broker.py
+++ Proyectos/Proyecto1/MOM/message_broker.py
@@ -6,8 +6,8 @@ messages_queue = []
 username='JJ'   
 
 class MessageBroker:
-    def __init__(self,topic_name,message):
-        #self.exchange=Exchange()
+    def __init__(self,topic_name,message,publisher_topic_list,subscriber_topic_list):
+        super().__init__(publisher_topic_list,subscriber_topic_list,topic_name)
         self.message_queue=MessageQueue()
         self.topic_name=topic_name
         self.message=message
--- Proyectos/Proyecto1/MOM/message_queue.py
+++ Proyectos/Proyecto1/MOM/message_queue.py
@@ -4,7 +4,7 @@ class MessageQueue:
     def __init__(self):
         # Creación de colas
         #para cada tópico una cola
-        self.topic_queue = []
+
         self.dictionary={}
 
 #Determina si las colas están vacias (False) o no (True)
@@ -14,11 +14,12 @@ class MessageQueue:
 #Agrega un nuevo elemento a la cola y al diccionario para asignarle un valor por mensaje
     def enqueue(self, message,topic_name):
         self.topic_queue.append(message)
-        self.save_to_file("message_queue.json")
-        if message in self.dictionary:
+        
+        if topic_name in self.dictionary:
             self.dictionary[topic_name].append(message)
         else:
             self.dictionary[topic_name] = [message]
+        self.save_to_file("message_queue.json")
         print(f"El mensaje: {message} ha sido agregado")
 
 #elimina y devuelve con el pop el elemento de la cola
@@ -31,10 +32,11 @@ class MessageQueue:
         print(self.topic_queue)
 
 #metodo para recibir los mensajes de una cola
-def get_messages_from_topic(self,topic_name):
-    with open('message_queue.json') as f:
-        data = json.load(f)
-    return data[topic_name]
+    def get_messages_from_topic(self,topic_name):
+        with open('message_queue.json') as f:
+            data = json.load(f)
+        return data[topic_name]
+
 #esto es para coger un mensaje en particular de la cola y enviarlo a un usuario
     def get_message(self, position):
         if position < 0 or position >= len(self.topic_queue):
@@ -53,9 +55,9 @@ def get_messages_from_topic(self,topic_name):
 #para términos de persistencia se guardarán los datos en un .json
     def save_to_file(self, queue_back):
         #se crea otro diccionario para guardar los datos que tiene la cola y el diccionario 
-        queue_data = {'queues': self.topic_queue, 'dictionary': self.dictionary}
+        queue_data = self.dictionary
         with open(queue_back, 'w') as f:
-            json.dump(queue_data, f)
+            json.dump(queue_data, f,indent=4)
             print("Datos guardados en el archivo", queue_back)
 
 
@@ -63,7 +65,6 @@ def get_messages_from_topic(self,topic_name):
     def load_from_file(self, queue_back):
             with open(queue_back, 'r') as f:
                 queue_data = json.load(f)
-                self.topic_queue = queue_data['queues']
                 self.dictionary = queue_data['dictionary']
             print("Datos cargados del archivo", queue_back)
 
